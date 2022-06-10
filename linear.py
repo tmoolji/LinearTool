b533e54292c60d86ecab8edf5f7c5e0e72eeb064
Saif Moolji <smoolji@berkeley.edu>
2022-06-10 17:28:00 -0400

    tranpose done

--- linear.py
+++ linear.py
@@ -5,3 +5,17 @@ def identity(n):
     for i in range(n):
         retval[i][i] = 1
     return retVal
+
+
+
+def transpose(input):
+    num_rows = len(input)
+    num_cols = len(input[0])
+    x = []
+    for i in range(num_cols):
+        x += [[input[m][i] for m in range(num_rows)]]
+
+    return x
+
+
+def guassian_elim()
