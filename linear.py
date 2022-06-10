97b559c5427f568cb64037fd44b48e1bd9083b86
Taimur Moolji <Taimur Moolji>
2022-06-10 15:54:51 -0400

    corrected identity and built working multiply functions

--- linear.py
+++ linear.py
@@ -1,7 +1,23 @@
 #Linear Tool
 
 def identity(n):
-    retVal = [0] * n * n
+    retVal = [[0] * n for _ in range(n)]
     for i in range(n):
-        retval[i][i] = 1
+        retVal[i][i] = 1
     return retVal
+
+
+def multiply(m, n):
+    retVal = [[0] * len(m) for _ in range(len(n[0]))]
+
+    for i in range(len(retVal)):
+        for j in range(len(retVal[0])):
+            sum = 0
+            for x in range(len(m[0])):
+                sum += m[i][x]*n[x][j]
+            retVal[i][j] = sum
+
+    return retVal
+
+
+def multiply_intermediate(m, n):
